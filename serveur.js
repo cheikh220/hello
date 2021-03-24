a2e0be422c0d0549178ebc5f9feed32fb652c35f
cheikh220 <81273231+cheikh220@users.noreply.github.com>
2021-03-24 10:58:48 +0100

    Add files via upload

---
+++ serveur.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+var nouveauPatient = new mongoose.Schema({
+    lastName: String,
+    firstName: String,
+    date: Number
+});
+var User = mongoose.model("User", nouveauPatient);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("enregistrement effectuer merci");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
